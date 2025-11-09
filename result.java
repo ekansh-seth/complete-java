08f06628de5621fb5ca00ec5758bd50b11741a9b
EKANSH_1111 <sethekansh607@gmail.com>
2025-11-09 22:53:51 +0530

    practice scanner class and object creation

---
+++ result.java
@@ -0,0 +1,13 @@
+package mypackage{
+    public class main{
+//java .util
+    }
+    public static void main (String[]args){
+        int name ;
+        int age ;
+        
+         scanner sc = new scanner
+         ekansh e= new ekansh;
+         
+    }
+}
