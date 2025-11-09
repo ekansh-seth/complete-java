f6d6f87e35eb0a8994604e06ee34b77d0882bdea
EKANSH_1111 <sethekansh607@gmail.com>
2025-11-09 23:02:45 +0530

    how to use for each in multi array

---
+++ multyarray.java
@@ -0,0 +1,17 @@
+public class multyarray {
+    public static void main(String[]args){
+        int [] marks; // 1 -D array;
+        int [][] flats;// 2-D array;
+        flats = new int [2] [3];
+flats[0][0] = 100;
+flats [0][1] = 101;
+flats [0][2] = 102;
+flats [0][3]= 103;
+
+    System.out.println("print a 2 d element ");
+    for(int i = 0;i<flats.length; i ++){// condition 
+        System.out.println(flats[i][j]);///forv each loop 
+
+    }
+    }
+}
