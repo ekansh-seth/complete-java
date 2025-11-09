d4c7269b8f688d4f8b95bce9e94e90b40f81a5b5
EKANSH_1111 <sethekansh607@gmail.com>
2025-11-09 22:51:22 +0530

    problem 3 for usings loops

---
+++ practice3.java
@@ -0,0 +1,20 @@
+public class practice3 {
+    public static void main(String[] args) {
+        // write a program to print to 10 in reverse order 
+        int n = 10;
+        for(i = 0 ; i<n; i ++){
+            for(i = o; i>n ; i--){
+
+            }
+            System.out.println("%d;*d;=,n,i,\n%d);
+            
+
+
+        }
+
+    
+
+        
+    }
+      
+}
