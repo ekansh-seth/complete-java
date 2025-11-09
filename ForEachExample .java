aff7540dc60ce7fda441cba558b8f74199e7499f
EKANSH_1111 <sethekansh607@gmail.com>
2025-11-09 23:01:38 +0530

    for each loop better understanding

---
+++ ForEachExample .java
@@ -0,0 +1,14 @@
+public class ForEachExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Using for-each loop:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
+///for(int num,numbers){
+/// } thuis for whole array traversal 
+/// System.out.println(num); for whole arrays number print 
+/// this is and best exmple to understand 
