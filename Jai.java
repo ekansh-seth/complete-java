ca9e91b09bf68b84861a7c79583447c19e2936d6
EKANSH_1111 <sethekansh607@gmail.com>
2025-11-09 22:45:40 +0530

    this is first over view of java

---
+++ Jai.java
@@ -0,0 +1,30 @@
+//blic class ekansh{
+    //blic static void main(String[] args)
+/
+
+      //System.out.println("hello world");
+//
+//
+
+//eak;//
+//pascal and camel case naming structure 
+
+//AddTwoNumbers = p//ascal nbaming c heratere 
+//addTwoNumber == camel case structure
+  
+
+//pascal for clsasses
+//camel for function
+public class Jai{
+
+}
+
+}
+    public static void main(String[]args);
+
+
+
+    system.out.println(x;"hellow");
+
+    
+    }
