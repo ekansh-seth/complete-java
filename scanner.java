a9d6a8509af18fc08b6101802900933aa06fd0ae
EKANSH_1111 <sethekansh607@gmail.com>
2025-11-09 22:53:08 +0530

    user input in java

---
+++ scanner.java
@@ -0,0 +1,12 @@
+public class scanner {
+   public static  void main(String[]args){
+    scanner.sc new scanner());
+    System.out.print("enter the name");
+    String name =sc.nextline();
+    System.out.print("hellow",+ name);
+    
+
+
+
+   }
+}
