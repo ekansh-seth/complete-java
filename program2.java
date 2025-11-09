b42e354d713ca725a37d66ae6d2e96e09ff92f9e
EKANSH_1111 <sethekansh607@gmail.com>
2025-11-09 22:50:26 +0530

    problem for better understanding for if and else

---
+++ program2.java
@@ -0,0 +1,16 @@
+public class program2 {
+    public static void main(String[] args){    
+        int age = 17;
+
+        if (age>=18){
+            System.out.println("beta gadi chala sktta hai ");
+
+    }
+        else{
+            System.out.println("bkl chote ho ");
+
+    }
+    
+}
+
+}
